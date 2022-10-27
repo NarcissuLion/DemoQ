cb515c1425ae54c7a68b53a8f785e93a2c77d530
JukkaLv <jukka.lv@hotmail.com>
2022-10-27 18:35:18 +0800

    Update UnityDefines.lua

--- Lua/UnityDefines.lua
+++ Lua/UnityDefines.lua
@@ -4,6 +4,7 @@ Vector3 = CS.UnityEngine.Vector3
 Vector2 = CS.UnityEngine.Vector2
 Quaternion = CS.UnityEngine.Quaternion
 SpriteRenderer = CS.UnityEngine.SpriteRenderer
+Sprite = CS.UnityEngine.Sprite
 Color = CS.UnityEngine.Color
 
 -- DOTween
